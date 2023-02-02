1b362fbfd0b7870f9e1fa72c01b2e178bbce19ec
Nathan Altice <naltice@ucsc.edu>
2023-02-01 16:44:12 -0800

    3.55 update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // Nathan Altice
 // Created: 5/13/18
 // Ported to Phaser 3: 5/9/20
-// Updated: 5/15/20
+// Updated: 5/15/23
 // Dialogging
-// Simple "teletext-style" dialog example that reads from a JSON file
+// Visual novel "teletext-style" dialog example that reads from a JSON file
 
 // TO-DO: integrate the end-of-line fix from ourcade: https://blog.ourcade.co/posts/2020/phaser-3-typewriter-text-effect-bitmap/
 
